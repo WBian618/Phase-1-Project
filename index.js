f668fe21e01f14c0932a5d582fba604a5df7178c
webster <wbian618@gmail.com>
2021-10-25 10:08:32 -0400

    JS on click events and info showing in display

--- index.js
+++ index.js
@@ -10,10 +10,21 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
         const recipeMenu = document.querySelector('#recipe-display')
 
-        recipeMenu.append.apply(img)
+        recipeMenu.append(img)
 
         img.addEventListener('click', () => {
+            const mainImage = document.querySelector('.detail-image')
+            const mainName = document.querySelector('#recipe-name')
+            const prepTime = document.querySelector('#prep-time')
+            const cookTime = document.querySelector('#cook-time')
+            const recipeLink = document.querySelector('#recipe-link')
             
+            recipeLink.innerText = mealsObj.link
+            cookTime.innerText = mealsObj.cook
+            prepTime.innerText = mealsObj.prep
+            mainName.innerText = mealsObj.name
+            mainImage.src = mealsObj.image
+            mainImage.alt = mealsObj.name
         })
     }))
 })
