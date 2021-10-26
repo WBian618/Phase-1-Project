5bc8c24dc759c666d1584906e3e76f98a911802d
webster <wbian618@gmail.com>
2021-10-26 10:32:12 -0400

    beef button working

--- index.js
+++ index.js
@@ -3,25 +3,21 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM Loaded')
         let chickButt = document.querySelector('#chickenButt')
-    
+        let beefButt = document.querySelector('#beefButt')
+        
         chickButt.addEventListener('click', () => {
         fetch('http://localhost:3000/chicken')
         .then(resp => resp.json())
         .then(data => data.forEach((chickObj) => {
             let chickImg = document.createElement('img')
             chickImg.src = chickObj.image
-            
             const chickDisplay = document.querySelector('#recipe-display')
-            chickDisplay.append(chickImg)
-            
+            chickDisplay.append(chickImg);
         
-
         
     chickImg.addEventListener('click', () => {
-        
     const display = document.querySelector('.detail-image')
     display.src = chickObj.image
-
     const recipeName = document.querySelector("#recipe-name")
     recipeName.innerText = chickObj.name
     const prep = document.querySelector('#prep-time')
@@ -34,7 +30,31 @@ document.addEventListener('DOMContentLoaded', (event) => {
    
 }))   
 })
-    const newRecipe = document.getElementById('new-recipe')
+    beefButt.addEventListener('click', () => {
+        fetch('http://localhost:3000/beef')
+        .then(resp => resp.json())
+        .then(data => data.forEach((beefObj) => {
+            let beefImg = document.createElement('img')
+            beefImg.src = beefObj.image
+            const beefDisplay = document.querySelector('#recipe-display')
+            beefDisplay.append(beefImg)
+
+    beefImg.addEventListener('click', () => {
+        const display = document.querySelector('.detail-image')
+    display.src = beefObj.image
+    const recipeName = document.querySelector("#recipe-name")
+    recipeName.innerText = beefObj.name
+    const prep = document.querySelector('#prep-time')
+    prep.innerText = beefObj.prep
+    const cook = document.querySelector('#cook-time')
+    cook.innerText = beefObj.cook
+    const link = document.querySelector('#recipe-link')
+    link.innerText = beefObj.link
+    })
+        }))
+    })
+
+const newRecipe = document.getElementById('new-recipe')
     newRecipe.addEventListener('submit', (e) => {
         e.preventDefault()
         const newRecipeObject = {}
